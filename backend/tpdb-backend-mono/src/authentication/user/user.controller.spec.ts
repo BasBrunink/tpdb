111e165c3391f192b28a684d6fe8edcda0749968
Bas Brunink <b.brunink@eaglescience.nl>
2024-11-19 15:39:27 +0100

    [TEST] userController 100%

--- backend/tpdb-backend-mono/src/authentication/user/user.controller.spec.ts
+++ backend/tpdb-backend-mono/src/authentication/user/user.controller.spec.ts
@@ -1,20 +1,56 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
 
 describe('UserController', () => {
-  let controller: UserController;
+  let userController: UserController;
+  let userService: jest.Mocked<UserService>;
+
+  const mockUserService = {
+    create: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
-      providers: [{ provide: UserService, useValue: {} }],
+      providers: [{ provide: UserService, useValue: mockUserService }],
     }).compile();
 
-    controller = module.get<UserController>(UserController);
+    userController = module.get<UserController>(UserController);
+    userService = module.get<UserService>(
+      UserService,
+    ) as jest.Mocked<UserService>;
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(userController).toBeDefined();
+  });
+  describe('create', () => {
+    it('should call userService.create with the correct parameters and return the created user', async () => {
+      // Arrange
+      const createUserDto: CreateUserDto = {
+        username: 'testuser',
+        email: 'test@example.com',
+        password: 'password123',
+      };
+      const mockUser: User = new User();
+
+      mockUser.id = '1';
+      mockUser.username = 'testuser';
+      mockUser.email = 'test@example.com';
+      mockUser.password = 'password123';
+      mockUser.salt = '';
+
+      mockUserService.create.mockResolvedValue(mockUser);
+
+      // Act
+      const result = await userController.create(createUserDto);
+
+      // Assert
+      expect(userService.create).toHaveBeenCalledWith(createUserDto);
+      expect(result).toEqual(mockUser);
+    });
   });
 });
