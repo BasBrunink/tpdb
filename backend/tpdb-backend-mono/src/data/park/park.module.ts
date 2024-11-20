dc1a35346c5ee7c5520e20b5811c695614a19f93
Bas Brunink <b.brunink@eaglescience.nl>
2024-11-20 16:26:01 +0100

    [Tests] some issues solved

--- backend/tpdb-backend-mono/src/data/park/park.module.ts
+++ backend/tpdb-backend-mono/src/data/park/park.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ParkService } from './park.service';
 import { ParkController } from './park.controller';
-import { ParkTypeController } from './park-type/park-type.controller';
-import { ParkTypeService } from './park-type/park-type.service';
 import { ParkTypeModule } from './park-type/park-type.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Park } from './entities/park.entity';
