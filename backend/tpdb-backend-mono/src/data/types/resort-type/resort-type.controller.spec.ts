d38dabfbb011d6e93a693e1f50757351b875c491
Bas Brunink <b.brunink@eaglescience.nl>
2024-11-25 10:39:24 +0100

    [WIP] resort type tests fixed is now a baseline

--- backend/tpdb-backend-mono/src/data/types/resort-type/resort-type.controller.spec.ts
+++ backend/tpdb-backend-mono/src/data/types/resort-type/resort-type.controller.spec.ts
@@ -4,14 +4,17 @@ import { ResortTypeService } from './resort-type.service';
 
 describe('ResortTypeController', () => {
   let controller: ResortTypeController;
+  let service: ResortTypeService;
 
+  const mockService = {};
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ResortTypeController],
-      providers: [ResortTypeService],
+      providers: [{ provide: ResortTypeService, useValue: mockService }],
     }).compile();
 
     controller = module.get<ResortTypeController>(ResortTypeController);
+    service = module.get<ResortTypeService>(ResortTypeService);
   });
 
   it('should be defined', () => {
--- backend/tpdb-backend-mono/src/data/types/resort-type/resort-type.service.spec.ts
+++ backend/tpdb-backend-mono/src/data/types/resort-type/resort-type.service.spec.ts
@@ -1,12 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ResortTypeService } from './resort-type.service';
+import { User } from '../../../authentication/user/entities/user.entity';
+import { Repository } from 'typeorm';
+import { ResortType } from './entities/resort-type.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+
+const mockRepo = {
+  create: jest.fn(),
+  save: jest.fn(),
+  find: jest.fn(),
+  findOneBy: jest.fn(),
+  delete: jest.fn(),
+};
+
+const mockUser: User = {
+  id: 'user123',
+  username: 'testuser',
+  email: 'test@example.com',
+  password: 'hashedpassword',
+};
 
 describe('ResortTypeService', () => {
   let service: ResortTypeService;
+  let repo: Repository<ResortType>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ResortTypeService],
+      providers: [ResortTypeService, {provide: getRepositoryToken(ResortType), useValue: mockRepo}],
     }).compile();
 
     service = module.get<ResortTypeService>(ResortTypeService);
