b88bb6c01f88a84054e3851bce08d5510a12543e
BasBrunink <bas.brunink@outlook.com>
2025-06-03 17:33:41 +0200

    App boots with a H2 DB

--- tpdb-backend/tpdb-boot/src/main/java/com/tpdb/boot/TpdbApplication.java
+++ tpdb-backend/tpdb-boot/src/main/java/com/tpdb/boot/TpdbApplication.java
@@ -2,7 +2,11 @@ package com.tpdb.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(basePackages = "com.tpdb.infrastructure.repository.jpa")
+@EntityScan(basePackages = "com.tpdb.infrastructure.repository.entity")
 @SpringBootApplication(scanBasePackages = {
         "com.tpdb.domain",
         "com.tpdb.application",
